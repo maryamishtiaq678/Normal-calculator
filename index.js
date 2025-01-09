fd6c6d468d2721c6d9e87d9ab66ca667368681f4
Maryam Ishtiaq <ranamaryam090@gmail.com>
2025-01-09 13:55:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function add()
+		{
+			var a=eval(document.m1.t1.value);
+			var b=eval(document.m1.t2.value);
+			var c=a+b;
+			document.m1.t3.value=c;
+		}
+
+		function sub()
+		{
+			var a=eval(document.m1.t1.value);
+			var b=eval(document.m1.t2.value);
+			var c=a-b;
+			document.m1.t3.value=c;
+		}
+              
+               function mul()
+		{
+			var a=eval(document.m1.t1.value);
+			var b=eval(document.m1.t2.value);
+			var	c=a*b;
+			document.m1.t3.value=c;
+		}
+
+		function div()
+		{
+			var a=eval(document.m1.t1.value);
+			var b=eval(document.m1.t2.value);
+			var c=a/b;
+			document.m1.t3.value=c;
+		}
